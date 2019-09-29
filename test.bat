c903aca05b1f4008994f5daa0281ceee6da00c2f
Pablo Potat0 <aadewale123@gmail.com>
2019-09-29 19:21:15 -0400

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,9 +1,12 @@
 @echo off
 
+//Get psexec on your computer
 set list=INSTRUCTOR AUTO11 AUTO12 AUTO21 AUTO22 AUTO31 AUTO32 AUTO41 AUTO42 AUTO51 AUTO52 AUTO61 AUTO62 AUTO71 AUTO72 DECEP11 DECEP12 DECEP21 DECEP22 DECEP31 DECEP32 DECEP41 DECEP42 DECEP51 DECEP52 DECEP61 DECEP62 MAX11 MAX12 MAX21 MAX22 MAX31 MAX32 MAX41 MAX42 MAX51 MAX52 MAX61 MAX62
 for %%a in (%list%) do (
-	Powershell.exe -Command "iwr -Uri https://raw.githubusercontent.com/emmaunel/funlab/master/new.jpg -OutFile C:\Users\student\Desktop\new.bmp // have to use psexec for remote execution
+	psexec.exe \\* iwr -Uri https://raw.githubusercontent.com/emmaunel/funlab/master/new.jpg -OutFile C:\Users\student\Desktop\new.bmp // have to use psexec for remote execution
 	Powershell.exe -Command "sleep 10"
+	//Powershell.exe -Command "iwr -Uri https://raw.githubusercontent.com/emmaunel/funlab/master/new.jpg -OutFile C:\Users\student\Desktop\new.bmp // have to use psexec for remote execution
+	//Powershell.exe -Command "sleep 10"
 	Powershell.exe -Command "msg * /SERVER:%%a Hello, Friend"
 	Powershell.exe -Command "sleep 5"
 	Powershell.exe -Command "msg * /SERVER:%%a HI, I hope you like you new wallpaper"
